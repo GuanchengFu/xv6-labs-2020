4334b8c16efb3f113286e05214d30a42aac5d5b7
fuguancheng <litstar414@gmail.com>
2021-08-20 19:31:36 +0800

    Add some new fields in proc, to support alarm (not initialized)

--- kernel/proc.h
+++ kernel/proc.h
@@ -97,6 +97,9 @@ struct proc {
   // these are private to the process, so p->lock need not be held.
   uint64 kstack;               // Virtual address of kernel stack
   uint64 sz;                   // Size of process memory (bytes)
+  uint64 handler_func;         // The handler function address, null if not specified.
+  int interval;                // The interval for alarm function.
+  int tick_after;              // This field tracks how many ticks have passed after the last call.
   pagetable_t pagetable;       // User page table
   struct trapframe *trapframe; // data page for trampoline.S
   struct context context;      // swtch() here to run process
